22333ce3dfcd65d39e66756c8efca89b868aac45
IT20014254 <it20014254@my.sliit.lk>
2023-10-17 09:54:22 +0530

    Registration button issue fixed

--- app/src/main/java/com/example/ead_mobile_app/LoginActivity.java
+++ app/src/main/java/com/example/ead_mobile_app/LoginActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +19,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
-    private Button btnRegistration;
+    private TextView btnRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnRegistration = findViewById(R.id.btnRegistration);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +67,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to navigate to the RegistrationActivity
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 //        btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +86,8 @@ public class LoginActivity extends AppCompatActivity {
 //                startActivity(intent);
 //            }
 //        });
+
+
     }
 
     private boolean authenticate(String username, String password) {
